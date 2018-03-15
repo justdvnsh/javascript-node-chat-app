ba97c9173dd6ada894fb25a89a94f80acad46a57
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-03-15 17:42:39 +0530

    events

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 let app = express();
-
+let server = http.createServer(app);
+let io = socketIO(server);
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.listen(8000, () => {
-  console.log('App running on port 8000')
-})
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(8000, () => {
+  console.log(`Server is up on 8000`);
+});
