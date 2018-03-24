bf9ceb48464564f2b770bcca5a0361f474eb0bdc
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-03-24 14:00:45 +0530

    Message emitting and broadcasting events

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message')
+
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
@@ -10,6 +12,15 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
+
+  socket.on('CreateMessage', (msg) => {
+    console.log('msg to server', msg)
+    io.emit('newMessage', generateMessage(msg.from, msg.text))
+  })
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
